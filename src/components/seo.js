827ec3b2d84113fafbd06dc76cc46417d741b71a
Peter Durham <peterdurham@protonmail.com>
2020-06-28 19:47:29 -0700

    updated og image

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
@@ -22,12 +15,19 @@ function SEO({ description, lang, meta, title }) {
             author
           }
         }
+        logo: file(absolutePath: { regex: "/rocketimage.jpg/" }) {
+          childImageSharp {
+            fluid(maxWidth: 400) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     `
   )
 
   const metaDescription = description || site.siteMetadata.description
-  const OGImage = `https://www.nodeupdate.com` + OGPath
+  const OGImage = `https://nodeupdate.com${logo.childImageSharp.fluid.src}`
   return (
     <Helmet
       htmlAttributes={{
